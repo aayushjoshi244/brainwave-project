ca9d6d77282f45957dd69b512ab0a3b309523542
aayushjoshi244 <aayush09345@gmail.com>
2024-07-30 23:27:47 +0530

    Generating Section

--- src/components/design/Header.jsx
+++ src/components/design/Header.jsx
@@ -28,11 +28,7 @@ export const BackgroundCircles = () => {
   );
 };
 
-<<<<<<< HEAD
-export const HamburgerMenu = () => {
-=======
-export const HambugerMenu = () => {
->>>>>>> 70ff497d66e3b7094afd4c86c1955e246862b6ab
+export const HamburgerMenu = () => {  // Corrected the component name here
   return (
     <div className="absolute inset-0 pointer-events-none lg:hidden">
       <div className="absolute inset-0 opacity-[.03]">
