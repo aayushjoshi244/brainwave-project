214b065b1d3e4bde34e29750e21abdb13263e833
aayushjoshi244 <aayush09345@gmail.com>
2024-08-04 14:27:02 +0530

    Stating Pricing section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Benefits from "./components/Benefits";
 import Collaboration from "./components/Collaboration";
+import Services from "./components/Services";
+import Pricing from "./components/Pricing";
 
 const App = () => {
   return (
@@ -11,8 +13,10 @@ const App = () => {
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
         <Hero />
-        <Benefits/>
-        <Collaboration/>
+        <Benefits />
+        <Collaboration />
+        <Services />
+        <Pricing />
       </div>
       <ButtonGradient />
     </>
---
+++ src/components/PricingList.jsx
@@ -0,0 +1,9 @@
+const PricingList = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PricingList
